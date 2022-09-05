29eac8c9a039a201a4670f19ce1843f10e1b89a9
Sebastian Chiterer <sebachiterer@gmail.com>
2022-09-05 12:00:03 -0300

    Avanzando con el proyecto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer';
+import Home from './pages/Home/Home';
+import DetailMovies from './pages/DetailMovies/DetailMovies';
+import NotFound from './pages/NotFound/NotFound';
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <>
     <Header/>
+
+    <Switch>
+          <Route path="/" exact >
+            <Home />
+          </Route>
+          {/* <Route path="/about" component={Characters} /> */}
+          <Route path="/movies/id/:id" component={DetailMovies} />
+          {/* <Route path="/contact" component={Contact}/> */}
+          <Route component={NotFound}/>
+    </Switch>
+
     <Footer/>
     </>
   );
