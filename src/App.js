ec2c182bfdd762d2eff6f503dce194ca578b34cb
Sebastian Chiterer <sebachiterer@gmail.com>
2022-09-05 11:29:26 -0300

    Avanzando

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <p>React</p>
+    <>
+    <Header/>
+    <Footer/>
+    </>
   );
 }
 
