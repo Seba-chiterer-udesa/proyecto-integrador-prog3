dfa6e8b3f2e3c48dd06d7ebacff17e4bcf6a2123
cristiano Banfi <cbanfi@udesa.edu.ar>
2022-09-15 18:23:36 -0300

    Haciendo favoritos

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -11,10 +11,18 @@ class Home extends Component {
       cargando: true,
       populares: [],
       encartel: [],
+      favoritos: []
     };
   }    
 
  componentDidMount(){
+
+    if (localStorage.length > 0) {
+     this.setState({ favoritos: JSON.parse(localStorage.getItem('favoritos')) || [''] })
+   } else {
+     localStorage.setItem('favoritos', JSON.stringify(this.state.favoritos))
+   }
+
     const populares = "https://api.themoviedb.org/3/movie/popular?api_key=fcb65972de75954111563f90b05f9fed"
     fetch(populares)
         .then((res)=> res.json())
@@ -38,6 +46,18 @@ class Home extends Component {
     
  }
 
+ handleFavoritos(card){
+  if (this.state.favoritos.some(fav => card.id === fav.id)) {
+      this.setState({favoritos: this.state.favoritos.filter(item => item.id !== card.id)}, () => {//asincronismo del this.State ",()"
+          localStorage.setItem("favoritos", JSON.stringify(this.state.favoritos))
+      })
+      console.log(this.state.favoritos.filter(item => item.id !== card.id))
+  } else {                                                               
+      this.setState({favoritos: [...this.state.favoritos, card]}, () => { //el "..." significa traeme todo lo que estaba en el array favoritos y el ", card" significaa que le agrego la nueva card que te estoy pasando
+          localStorage.setItem("favoritos", JSON.stringify(this.state.favoritos))
+      })
+  }
+}
 
   render() {
     return ( 
@@ -51,7 +71,7 @@ class Home extends Component {
              <p>Cargando</p>
             ) : (
             this.state.populares.map(pelicula =>(
-               <Card key={pelicula.id} pelicula={pelicula}/>)
+               <Card key={pelicula.id} pelicula={pelicula} favorito={(pelicula)=> this.handleFavoritos(pelicula)}/>)
            )
            )  
             }      
